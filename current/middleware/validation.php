6486e1d160f7009c6fa5e0ac3eeb7299b5f0193a
ZanMark <102849801+Sandatang@users.noreply.github.com>
2024-04-15 21:10:27 +0800

    MY_CRUD_BOILERPLATER

--- current/middleware/validation.php
+++ current/middleware/validation.php
@@ -3,5 +3,24 @@
 include('../config/app.php');
 include('../controller/EventController.php');
 
+// ADD
+if(isset($_POST['add-btn'])){
+    $data = [
+        'name' => $_POST['name'],
+        'email' => $_POST['email'],
+        'password' => $_POST['password'],
+    ];
+
+    $controller = new EventController();
+    $result = $controller->addUser($data);
+
+    if($result){
+        redirect('added successfully', '../client/index.php');
+    }else{
+        redirect('Somethign went wrong, try again later', 'add.php');
+    }
+
+}
+
 
 ?>
